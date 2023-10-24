967658c989a908efaae95469e67934221983a184
Jamie Sewell <jamiegsewell@gmail.com>
2023-10-24 15:36:01 +0100

    creates functions for iteration and user picks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,56 @@
+/**
+ * computer choices and function to choose randomly
+ */
 let options = ["rock", "paper", "scissors", "lizard", "spock"]
 function computerChoice() {
     for (let i = 0; i > options.length; i++);
     i = Math.floor(Math.random() * 5);
     let compChoice = options[i];
-    return compChoice.value;
+    return compChoice;
 }
-console.log(computerChoice())
+
+/**
+ * iterate Wins
+ */
+function winCount() {
+
+}
+
+/**
+ * iterate lose
+ */
+function loseCount() {
+
+}
+
+/**
+ * iterate draw
+ */
+function drawCount() {
+
+}
+
+
+/**
+ * Code to decide who wins
+ */
+
+function rockPick() {
+
+}
+
+function paperPick() {
+
+}
+
+function scissorsPick() {
+
+}
+
+function lizardPick() {
+
+}
+
+function spockPick() {
+
+}
